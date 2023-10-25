cf61f4224861370484881ff2533e25152ca28b11
colburncodes <colburnsanders@gmail.com>
2023-10-25 14:57:02 -0500

    refactor: account repo

--- Social.Infrastructure/Repositories/AccountRepository.cs
+++ Social.Infrastructure/Repositories/AccountRepository.cs
@@ -1,21 +1,36 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Social.Core.Requests;
-using Social.Core.Responses;
+using Social.Infrastructure.Data;
+using Social.Infrastructure.Exceptions;
 using Social.Infrastructure.Interfaces;
+using User = Social.Core.Responses.User;
 
 namespace Social.Infrastructure.Repositories;
 
 public class AccountRepository : IAccountRespository
 {
+    private readonly SocialContext _dbContext;
     private ILogger<AccountRepository> _logger;
 
-    public AccountRepository(ILogger<AccountRepository> logger)
+    public AccountRepository(SocialContext dbContext, ILogger<AccountRepository> logger)
     {
+        _dbContext = dbContext;
         _logger = logger;
     }
 
-    public User RegisterUser(Register register)
+    public async Task<User> RegisterUserAsync(Register register)
     {
+        var userExists = await UserExistAsync(register);
+        if (userExists)
+        {
+            throw new UserExistException("User already exists!");
+        }
+        
+        // TODO: CREATE ACCOUNT METHOD WITH REGISTER OBJ
+        
+        // TODO: CREATE PASSHASH METHOD TO COMPUTE THE SALT AND HASH
+        
         var user = new User()
         {
             Bio = "Bio",
@@ -25,6 +40,17 @@ public class AccountRepository : IAccountRespository
             Token = "No Token"
         };
 
+        // TODO: HANDLE DB CONTEXT
+        // await _dbContext.Accounts.AddAsync(user);
+        // await _dbContext.SaveChangesAsync();
+
         return user;
+    }
+
+    private async Task<bool> UserExistAsync(Register register)
+    {
+        var result = await _dbContext.Accounts
+            .AnyAsync(a => a.Email == register.Email || a.User.UserName == register.UserName);
+        return result;
     }
 }
